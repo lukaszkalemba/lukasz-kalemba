b68f406d156637cf50173786f6c9355e67aaa08b
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-01-25 14:56:56 +0100

    Change folder structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from 'react';
+import Layout from '../assets/global/Layout/Layout';
+import SEO from '../assets/global/SEO/SEO';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Index page</h1>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
