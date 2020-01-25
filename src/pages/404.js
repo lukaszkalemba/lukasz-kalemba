3838a3d77a62ba121f648d805bd6e79f6cccbeea
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-01-25 14:56:56 +0100

    Change folder structure

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
-import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from 'react';
+import Layout from '../assets/global/Layout/Layout';
+import SEO from '../assets/global/SEO/SEO';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404</h1>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
