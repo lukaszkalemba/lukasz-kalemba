9497d0bbe680a26a7242713456da761be6f95aa9
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-01-25 15:03:28 +0100

    Configure gatsby-plugin-root-import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Layout from '../assets/global/Layout/Layout';
-import SEO from '../assets/global/SEO/SEO';
+import Layout from 'assets/global/Layout/Layout';
+import SEO from 'assets/global/SEO/SEO';
 
 const IndexPage = () => (
   <Layout>
