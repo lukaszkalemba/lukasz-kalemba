d9b93b5be9ddf25ce737fbd5c5ce483a24b12771
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-01-26 17:48:12 +0100

    Install styled-components and configure theme provider

--- src/assets/global/Layout/Layout.js
+++ src/assets/global/Layout/Layout.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
+import theme from 'utils/theme';
+import GlobalStyle from 'assets/global/GlobalStyle/GlobalStyle';
 
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </>
+  );
 };
 
 Layout.propTypes = {
---
+++ src/utils/theme.js
@@ -0,0 +1,11 @@
+import color from 'utils/color';
+import font from 'utils/font';
+import media from 'utils/media';
+import zindex from 'utils/zindex';
+
+export default {
+  color,
+  font,
+  media,
+  zindex,
+};
