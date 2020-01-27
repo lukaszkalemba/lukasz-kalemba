f493f4b3a1895363300eaf675ea6c690c6ce6eb2
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-01-27 23:40:12 +0100

    Create Icon components and create overlay template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from 'assets/global/Layout/Layout';
 import SEO from 'assets/global/SEO/SEO';
+import Overlay from 'templates/Overlay/Overlay';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>zażółć gęślą jaźń</h1>
+    <Overlay />
   </Layout>
 );
 
