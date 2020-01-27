6b947e1e1cd7b0910d92b76fd64792b1cd9d2970
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-01-27 23:56:04 +0100

    Create styles for Overlay template

---
+++ src/templates/Overlay/StyledOverlay.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh);
+`;
+
+const Logo = styled.div`
+  width: 130px;
+`;
+
+export default { Wrapper, Logo };
