080f92f7d28b639881d2eb4d5eb7e7316dc00b10
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-01-27 23:56:04 +0100

    Create styles for Overlay template

--- src/templates/Overlay/Overlay.js
+++ src/templates/Overlay/Overlay.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Icon from 'assets/global/Icon/Icon';
 import logo from 'assets/images/logo_codemon.svg';
+import Styled from './StyledOverlay';
 
 const Overlay = () => {
   return (
-    <div>
-      <Icon src={logo} />
-    </div>
+    <Styled.Wrapper>
+      <Styled.Logo>
+        <Icon src={logo} />
+      </Styled.Logo>
+    </Styled.Wrapper>
   );
 };
 
