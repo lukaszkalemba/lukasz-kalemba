656c0b3899d58cd5092082d4c2e56f2a6240e45f
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-16 13:39:33 +0200

    Create and import Logo component

---
+++ src/components/commons/Logo/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Icon from 'components/commons/Icon';
+import logo from 'assets/images/logo_codemon.svg';
+
+const Logo = () => <Icon src={logo} />;
+
+export default Logo;
--- src/components/layout/Nav/Nav.js
+++ src/components/layout/Nav/Nav.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Logo from 'components/commons/Logo';
 
 const Nav = () => {
   return (
     <header>
+      <Logo />
       <nav>Nav</nav>
     </header>
   );
