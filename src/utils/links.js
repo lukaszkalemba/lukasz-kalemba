5e6289d71823450c7c0486cbbe99d15ebb88233d
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-16 14:07:14 +0200

    Create links utility file

---
+++ src/utils/links.js
@@ -0,0 +1,19 @@
+const links = [
+  {
+    id: 0,
+    path: '/projekty',
+    name: 'projekty',
+  },
+  {
+    id: 1,
+    path: '/usługi',
+    name: 'usługi',
+  },
+  {
+    id: 2,
+    path: '/o-nas',
+    name: 'o nas',
+  },
+];
+
+export default links;
