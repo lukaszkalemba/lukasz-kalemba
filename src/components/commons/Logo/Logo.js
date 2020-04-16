0bc4119ff13c861c14567e92dc869d0169574373
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-16 15:08:03 +0200

    Add link to the logo and create on hover animation

--- src/components/commons/Logo/Logo.js
+++ src/components/commons/Logo/Logo.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Icon from 'components/commons/Icon';
 import logo from 'assets/images/logo_codemon.svg';
 import S from './Logo.styles';
 
 const Logo = () => (
   <S.Wrapper>
-    <Link to="/">
+    <S.Link to="/">
       <Icon src={logo} />
-    </Link>
+    </S.Link>
   </S.Wrapper>
 );
 
--- src/components/commons/Logo/Logo.styles.js
+++ src/components/commons/Logo/Logo.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link as GatsbyLink } from 'gatsby';
 
 const Wrapper = styled.div`
   width: 120px;
@@ -8,4 +9,35 @@ const Wrapper = styled.div`
   `}
 `;
 
-export default { Wrapper };
+const Link = styled(GatsbyLink)`
+  ${({ theme }) => theme.media.desktop`
+    #logo-path-circle {
+      transition: transform 400ms ease-in-out;
+    }
+
+    #logo-path-slash {
+      transition: transform 400ms ease-in-out, opacity 400ms ease-in-out;
+    }
+
+    #logo-path-text {
+      transition: transform 400ms ease-in-out;
+    }
+
+    :hover {
+      #logo-path-circle {
+        transform: translate(90%, 25%) scale(0.8);
+      }
+
+      #logo-path-slash {
+        transform: translateX(63%);
+        opacity: 0;
+      }
+
+      #logo-path-text {
+        transform: translateX(-14%);
+      }
+    }
+  `};
+`;
+
+export default { Wrapper, Link };
