7d4df430a8975500843774cb905e8c54d54bdbc3
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-16 21:56:06 +0200

    Create Hamburger component and styles file for Nav component

--- src/components/layout/Nav/Nav.js
+++ src/components/layout/Nav/Nav.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Logo from 'components/commons/Logo';
 import Container from 'components/commons/Container';
 import NavItems from 'components/layout/Nav/NavItems';
+import Hamburger from 'components/layout/Nav/Hamburger';
+import S from './Nav.styles';
 
 const Nav = () => {
   return (
     <header>
       <Container axis="both">
-        <Logo />
+        <S.TopBar>
+          <Logo />
+          <Hamburger />
+        </S.TopBar>
         <nav>
           <NavItems />
         </nav>
---
+++ src/components/layout/Nav/Nav.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const TopBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export default { TopBar };
