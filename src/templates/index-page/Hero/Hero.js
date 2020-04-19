a46d2d7ba0bbb68653efb03d9bec0e12f57a7e59
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-19 16:45:10 +0200

    Create and import Button component

--- src/templates/index-page/Hero/Hero.js
+++ src/templates/index-page/Hero/Hero.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Container from 'components/commons/Container';
 import Heading from 'components/commons/Heading';
+import Button from 'components/commons/Button';
 
 const Hero = () => {
   return (
     <Container axis="both">
       <div style={{ height: '100vh' }}>
         <Heading tag="h1">Zaistniej w sieci</Heading>
+        <Button priority="primary">Bezpłatna wycena</Button>
+        <Button priority="secondary">Zrealizowane projekty</Button>
       </div>
     </Container>
   );
