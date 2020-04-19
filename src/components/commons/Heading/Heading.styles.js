e50a358dc1b0b771410a8c864707d80615943d79
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-19 15:38:54 +0200

    Create and import Heading component with styles

---
+++ src/components/commons/Heading/Heading.styles.js
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+
+const baseStyles = css``;
+
+const H1 = styled.h1`
+  ${baseStyles}
+`;
+
+const H2 = styled.h2`
+  ${baseStyles}
+`;
+
+const Heading = {
+  H1,
+  H2,
+};
+
+export default { Heading };
