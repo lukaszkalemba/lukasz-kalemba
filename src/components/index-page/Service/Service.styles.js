9a34671494bfa2145cefc7d58a31292274afe4e4
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-21 21:31:42 +0200

    Create styles for right bar on Services section

--- src/components/index-page/Service/Service.styles.js
+++ src/components/index-page/Service/Service.styles.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
-const Article = styled.article``;
+const Article = styled.article`
+  height: calc(100% / 3);
+  border: 1px solid ${({ theme }) => theme.color.primary};
+  display: flex;
+  align-items: flex-end;
+  min-height: 300px;
 
-const Title = styled.h3``;
+  ${({ theme }) => theme.media.laptop`
+    justify-content: flex-end;
+  `}
+`;
+
+const Title = styled.h3`
+  font-size: ${({ theme }) => theme.font.size.m};
+  margin: 0;
+  padding: 50px;
+
+  ${({ theme }) => theme.media.tablet`
+    font-size: ${theme.font.size.m};
+  `}
+`;
 
 export default { Article, Title };
