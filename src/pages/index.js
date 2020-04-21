2eee8c9668c594a7c6fe840ab01360b42640b09e
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-21 13:07:52 +0200

    Create and import LastProject section with heading

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Layout from 'components/layout/Layout';
 import SEO from 'components/commons/SEO';
 import Hero from 'templates/index-page/Hero';
+import LastProjects from 'templates/index-page/LastProjects';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="home" />
     <Hero />
+    <LastProjects />
   </Layout>
 );
 
---
+++ src/templates/index-page/LastProjects/LastProjects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'components/commons/Container';
+import Heading from 'components/commons/Heading';
+
+const LastProjects = () => {
+  return (
+    <Container axis="both">
+      <Heading>Ostatnie projekty.</Heading>
+    </Container>
+  );
+};
+
+export default LastProjects;
