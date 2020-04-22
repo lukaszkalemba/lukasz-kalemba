dfb2db1d2ca6413d4474151e0234c56fc762695f
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-22 21:06:08 +0200

    Create on hover animation for Button component

--- src/components/commons/Button/Button.styles.js
+++ src/components/commons/Button/Button.styles.js
@@ -15,6 +15,35 @@ const Button = styled.button`
   z-index: ${({ theme }) => theme.zindex.positive1};
   margin-bottom: 8px;
 
+  ::before {
+    content: '';
+    display: none;
+    width: 2%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    background-color: ${({ theme }) => theme.color.primary};
+    transition: transform 400ms ease-in-out;
+    transform-origin: 0 100%;
+
+    ${({ theme }) => theme.media.laptop`
+      display: block;
+    `}
+  }
+
+  :hover {
+    ${({ theme }) => theme.media.laptop`
+      ::before {
+        transform: translateX(calc(5000% - 5px));
+      }
+
+      span {
+        transform: translateX(-5px) skew(45deg);
+      }
+    `};
+  }
+
   ${({ theme }) => theme.media.tablet`
     font-size: ${theme.font.size.xxs};
     width: 230px;
@@ -29,6 +58,7 @@ const Button = styled.button`
 
   span {
     display: block;
+    transition: transform 400ms 100ms ease-in-out;
 
     ${({ theme }) => theme.media.laptop`
       transform: skew(45deg);
