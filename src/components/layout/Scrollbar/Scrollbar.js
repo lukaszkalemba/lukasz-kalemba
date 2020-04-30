62eb0732e9e607696199e9232d878c06cea3b214
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-30 15:41:58 +0200

    Edit Scrollbar component to use react-smooth-scrollbar

--- src/components/layout/Scrollbar/Scrollbar.js
+++ src/components/layout/Scrollbar/Scrollbar.js
@@ -1,24 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import Scroll from 'smooth-scrollbar';
+import React from 'react';
 import PropTypes from 'prop-types';
 import S from './Scrollbar.styles';
 
 const Scrollbar = ({ children }) => {
-  let scrollbar = useRef(null);
-
-  useEffect(() => {
-    Scroll.init(scrollbar, { damping: 0.05 });
-  }, []);
-
-  return (
-    <S.Scrollbar
-      ref={el => {
-        scrollbar = el;
-      }}
-    >
-      {children}
-    </S.Scrollbar>
-  );
+  return <S.Scrollbar damping={0.025}>{children}</S.Scrollbar>;
 };
 
 Scrollbar.propTypes = {
