0a1dc0e8e55a24c14bbfd6d18336dfae85998c95
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-05-05 21:36:09 +0200

    Change hero animations on mobile devices

--- src/components/layout/Scrollbar/Scrollbar.js
+++ src/components/layout/Scrollbar/Scrollbar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import S from './Scrollbar.styles';
 
 const Scrollbar = ({ children }) => {
-  return <S.Scrollbar damping={0.025}>{children}</S.Scrollbar>;
+  return <S.Scrollbar damping={0.075}>{children}</S.Scrollbar>;
 };
 
 Scrollbar.propTypes = {
