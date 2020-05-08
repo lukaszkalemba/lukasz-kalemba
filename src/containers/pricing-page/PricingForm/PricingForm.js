2115fcf1f8284fbdccbb8e3ae00e55d39ec3ef69
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-05-08 11:48:09 +0200

    Create and import pricing form container

---
+++ src/containers/pricing-page/PricingForm/PricingForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Heading from 'components/common/Heading';
+import Container from 'components/common/Container';
+
+const NotFound = () => {
+  return (
+    <Container axis="both">
+      <Heading>Opowiedz nam o swoim pomyśle.</Heading>
+    </Container>
+  );
+};
+
+export default NotFound;
