da7b3ba36bd35f6faa1600a8da21ac11588db293
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-05-08 12:34:19 +0200

    Import office image and style it

--- src/containers/pricing-page/PricingForm/PricingForm.js
+++ src/containers/pricing-page/PricingForm/PricingForm.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import Heading from 'components/common/Heading';
 import Container from 'components/common/Container';
 import S from './PricingForm.styles';
 
+const IMAGE_QUERY = graphql`
+  {
+    officeImage: file(relativePath: { eq: "photo_office.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+  }
+`;
+
 const NotFound = () => {
+  const { officeImage } = useStaticQuery(IMAGE_QUERY);
+
   return (
     <Container axis="both">
       <S.Wrapper>
-        <Heading>Opowiedz nam o swoim pomyśle.</Heading>
+        <div>
+          <Heading>Opowiedz nam o swoim pomyśle.</Heading>
+        </div>
+
+        <S.ImageWrapper>
+          <S.Image fluid={officeImage.childImageSharp.fluid} />
+        </S.ImageWrapper>
       </S.Wrapper>
     </Container>
   );
