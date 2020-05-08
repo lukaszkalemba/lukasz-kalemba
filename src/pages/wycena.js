597a2ef7ab2ea057bc363975188b8e56e9908e62
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-05-08 11:37:51 +0200

    Create pricing page file

---
+++ src/pages/wycena.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'components/layout/Layout';
+import SEO from 'components/common/SEO';
+
+const PricingPage = () => (
+  <Layout>
+    <SEO title="wycena" />
+    <h1>wycena</h1>
+  </Layout>
+);
+
+export default PricingPage;
