2ee81b19fa5fcb27ac1131db0214eaf3e4bc2edc
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-05-15 14:51:50 +0200

    Create styles for blog content

--- src/containers/blog-post-page/BlogPostContent/BlogPostContent.js
+++ src/containers/blog-post-page/BlogPostContent/BlogPostContent.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import parse from 'html-react-parser';
+import Container from 'components/common/Container';
 import S from './BlogPostContent.styles';
 
 const BlogPostContent = ({ content }) => {
   return (
     <S.Wrapper>
-      <p>{content}</p>
+      <Container axis="both">
+        <Container axis="x">
+          <S.Content>{parse(content)}</S.Content>
+        </Container>
+      </Container>
     </S.Wrapper>
   );
 };
--- src/containers/blog-post-page/BlogPostContent/BlogPostContent.styles.js
+++ src/containers/blog-post-page/BlogPostContent/BlogPostContent.styles.js
@@ -1,5 +1,57 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-bottom: 100px;
+`;
 
-export default { Wrapper };
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    font-size: ${({ theme }) => theme.font.size.s};
+    margin-top: 50px;
+    margin-bottom: 5px;
+
+    ${({ theme }) => theme.media.tablet`
+      font-size: ${theme.font.size.l};
+      margin-top: 70px;
+      margin-bottom: 10px;
+    `}
+  }
+
+  p {
+    font-size: ${({ theme }) => theme.font.size.xs};
+    max-width: 750px;
+    line-height: 25px;
+    opacity: 0.8;
+    margin-bottom: 30px;
+
+    ${({ theme }) => theme.media.desktop`
+      max-width: 1000px;
+    `}
+  }
+
+  p:nth-child(4n) {
+    text-align: right;
+    align-self: flex-end;
+  }
+
+  h2:nth-child(4n - 1) {
+    text-align: right;
+  }
+
+  h2:first-child {
+    margin-top: -50px;
+  }
+
+  p:last-child {
+    margin-bottom: -20px;
+
+    ${({ theme }) => theme.media.desktop`
+      margin-bottom: 30px;
+    `}
+  }
+`;
+
+export default { Wrapper, Content };
