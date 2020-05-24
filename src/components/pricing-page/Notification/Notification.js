87a6320256140913de16f71ef144cba5177eedb7
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-05-24 17:22:13 +0200

    Create and import basic Notification styles

--- src/components/pricing-page/Notification/Notification.js
+++ src/components/pricing-page/Notification/Notification.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Button from 'components/common/Button';
+import Heading from 'components/common/Heading';
+import S from './Notification.styles';
 
 const Notification = () => {
-  return <div>Notification.js</div>;
+  return (
+    <S.Wrapper>
+      <Heading>Poszło!</Heading>
+      <p>Odezwiemy się do Ciebie w ciągu 24.</p>
+      <Button priority="secondary" path="/">
+        Powrót do strony głównej
+      </Button>
+    </S.Wrapper>
+  );
 };
 
 export default Notification;
---
+++ src/components/pricing-page/Notification/Notification.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: ${({ theme }) => theme.zindex.positive3};
+  background-color: ${({ theme }) => theme.color.background.light};
+`;
+
+export default { Wrapper };
