1735338e6a5cc7336a3c8f29f7981c3e341340d4
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-06 19:00:22 +0200

    Create notification state

--- src/components/organisms/PricingContent/PricingContent.js
+++ src/components/organisms/PricingContent/PricingContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'components/particles/Container';
 import PricingImage from 'components/atoms/PricingImage';
 import PricingForm from 'components/molecules/PricingForm';
@@ -6,13 +6,20 @@ import Notification from 'components/organisms/Notification';
 import S from './PricingContent.styles';
 
 const PricingContent = () => {
+  const [submissionStatus, setSubmissionStatus] = useState(null);
+
   return (
     <Container axis="both" doubling="x">
       <S.Wrapper>
         <div>
           <S.Heading>Opowiedz nam o swoim pomyśle.</S.Heading>
-          <Notification />
-          <PricingForm />
+          {submissionStatus && (
+            <Notification
+              submissionStatus={submissionStatus}
+              setSubmissionStatus={setSubmissionStatus}
+            />
+          )}
+          <PricingForm setSubmissionStatus={setSubmissionStatus} />
         </div>
         <PricingImage />
       </S.Wrapper>
