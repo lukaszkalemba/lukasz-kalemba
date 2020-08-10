a1e183d184955d62aa4d5557806a20119693b467
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-10 16:14:24 +0200

    Change way of including storybook global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,22 @@
-import { addParameters } from '@storybook/react';
+import React from 'react';
+import { addDecorator, addParameters, configure } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { ThemeProvider } from 'styled-components';
+import theme from 'utils/theme';
+import GlobalStyle from 'components/particles/GlobalStyle';
 
 global.__PATH_PREFIX__ = '';
+
+configure(require.context('../src/components', true, /\.stories\.js$/), module);
+
+const GlobalWrapper = storyFn => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {storyFn()}
+  </ThemeProvider>
+);
+
+addDecorator(GlobalWrapper);
 
 addParameters({
   viewport: {
--- src/components/atoms/Logo/Logo.stories.js
+++ src/components/atoms/Logo/Logo.stories.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
-import { withDesign } from 'storybook-addon-designs';
-import themeDecorator from 'utils/themeDecorator';
 import Logo from './Logo';
 
-addDecorator(themeDecorator);
-
-export const primaryLogo = () => <Logo />;
-
-primaryLogo.story = {
-  name: 'Primary Logo',
-  parameters: {
-    design: {
-      type: 'figma',
-      url:
-        'https://www.figma.com/file/2o7dYUrPl9GFtAn7tAxzwj/.%2Fcodemon?node-id=2%3A9',
-    },
-  },
-};
+export const navigationLogo = () => <Logo />;
 
 export default {
   component: Logo,
-  decorators: [withDesign],
   title: 'Atoms|Logo',
 };
