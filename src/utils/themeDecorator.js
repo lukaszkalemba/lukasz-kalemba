610b61b7ec42f4eeaf322684b1e611ddce0d39ad
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-10 16:14:24 +0200

    Change way of including storybook global styles

--- src/utils/themeDecorator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import theme from 'utils/theme';
-
-const ThemeDecorator = storyFn => (
-  <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
-);
-
-export default ThemeDecorator;
