09080c831498c80d3c4e907ca939f615a9e54a32
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-14 13:59:26 +0200

    Create hamburger stories

---
+++ src/components/atoms/Hamburger/Hamburger.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Hamburger from './Hamburger';
+
+export const hamburger = () => <Hamburger />;
+
+export default {
+  component: Hamburger,
+  title: 'Atoms|Hamburger',
+};
