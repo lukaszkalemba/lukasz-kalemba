0f7f700774b581f8b1f0e3f9c0de62f3aad3f77c
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-14 13:16:06 +0200

    Create footer logo stories

---
+++ src/components/atoms/FooterLogo/FooterLogo.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import FooterLogo from './FooterLogo';
+
+export const footerLogo = () => <FooterLogo />;
+
+export default {
+  component: FooterLogo,
+  title: 'Atoms|FooterLogo',
+};
