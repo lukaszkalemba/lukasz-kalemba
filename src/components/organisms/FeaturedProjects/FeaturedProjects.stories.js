0b31320bceb5bf0002162302ba3ca493ca956bd4
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-16 18:37:08 +0200

    Create stories for featured projects component

---
+++ src/components/organisms/FeaturedProjects/FeaturedProjects.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import FeaturedProjects from './FeaturedProjects';
+
+export const featuredProjects = () => <FeaturedProjects />;
+
+export default {
+  component: FeaturedProjects,
+  title: 'Organisms|FeaturedProjects',
+};
