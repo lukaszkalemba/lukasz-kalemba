6739cc9cd7165288f83e3f4b1bb2fc4594dc6247
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-16 17:07:01 +0200

    Create stories for all blog posts component

---
+++ src/components/organisms/AllBlogPosts/AllBlogPosts.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AllBlogPosts from './AllBlogPosts';
+
+export const allBlogPosts = () => <AllBlogPosts />;
+
+export default {
+  component: AllBlogPosts,
+  title: 'Organisms|AllBlogPosts',
+};
