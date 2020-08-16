be5d2827872995761ca123efef3e999c14ef9b19
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-16 10:21:24 +0200

    Create stories for pricing image component

---
+++ src/components/atoms/PricingImage/PricingImage.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PricingImage from './PricingImage';
+
+export const pricingImage = () => <PricingImage />;
+
+export default {
+  component: PricingImage,
+  title: 'Atoms|PricingImage',
+};
