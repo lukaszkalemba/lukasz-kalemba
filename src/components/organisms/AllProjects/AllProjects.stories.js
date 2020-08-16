85a4ca2accda3b47d922d7dd07cb9e60710d0537
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-16 17:08:10 +0200

    Create stories for all projects component

---
+++ src/components/organisms/AllProjects/AllProjects.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AllProjects from './AllProjects';
+
+export const allProjects = () => <AllProjects />;
+
+export default {
+  component: AllProjects,
+  title: 'Organisms|AllProjects',
+};
