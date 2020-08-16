eb111e3bbd8511ff1f47bf9922740863f766b180
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-16 18:26:18 +0200

    Create stories for not found component

---
+++ src/components/organisms/NotFound/NotFound.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NotFound from './NotFound';
+
+export const notFound = () => <NotFound />;
+
+export default {
+  component: NotFound,
+  title: 'Organisms|NotFound',
+};
