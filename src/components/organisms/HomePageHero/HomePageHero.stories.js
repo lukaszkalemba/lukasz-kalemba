4c7bff06841f7375f58ec3fae8872159e470455f
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-16 17:54:31 +0200

    Create stories for home page hero component

---
+++ src/components/organisms/HomePageHero/HomePageHero.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HomePageHero from './HomePageHero';
+
+export const homePageHomeHero = () => <HomePageHero />;
+
+export default {
+  component: HomePageHero,
+  title: 'Organisms|Hero',
+};
