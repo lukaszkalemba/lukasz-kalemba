f07c4091efcbdc9257628ca62841d014d1ff4ac1
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-17 13:32:56 +0200

    Create stories for project content component

---
+++ src/components/organisms/ProjectContent/ProjectContent.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProjectContent from './ProjectContent';
+
+export const projectContent = () => (
+  <ProjectContent
+    firstViewImage={{}}
+    secondViewImage={{}}
+    thirdViewImage={{}}
+  />
+);
+
+export default {
+  component: ProjectContent,
+  title: 'Organisms|ProjectContent',
+};
