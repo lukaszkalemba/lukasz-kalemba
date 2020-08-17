0c55d229e0fc2c260173f34b8f3b26bb37f2d3d9
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-17 16:18:36 +0200

    Create stories for pricing form component

---
+++ src/components/molecules/PricingForm/PricingForm.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PricingForm from './PricingForm';
+
+export const pricingForm = () => <PricingForm />;
+
+export default {
+  component: PricingForm,
+  title: 'Molecules|PricingForm',
+};
