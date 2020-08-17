7b1099c06a52e51fe63abbaa27a2d6641ae88013
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-17 15:58:47 +0200

    Create stories for services CTA section component

---
+++ src/components/molecules/ServicesCTASection/ServicesCTASection.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ServicesCTASection from './ServicesCTASection';
+
+export const servicesCTASection = () => <ServicesCTASection animation={{}} />;
+
+export default {
+  component: ServicesCTASection,
+  title: 'Molecules|ServicesCTASection',
+};
