342faecef65952ce05c2f5aee4e7dcc3ac5d94f7
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-17 16:38:15 +0200

    Create stories for footer links component

---
+++ src/components/molecules/FooterLinks/FooterLinks.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import FooterLinks from './FooterLinks';
+
+export const footerLinks = () => <FooterLinks />;
+
+export default {
+  component: FooterLinks,
+  title: 'Molecules|FooterLinks',
+};
