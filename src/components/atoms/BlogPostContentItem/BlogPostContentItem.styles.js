024d77a7fbc8422a0212480467ed0edfa6d09be0
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-17 13:27:32 +0200

    Fix blog post content styles bug

--- src/components/atoms/BlogPostContentItem/BlogPostContentItem.styles.js
+++ src/components/atoms/BlogPostContentItem/BlogPostContentItem.styles.js
@@ -25,27 +25,6 @@ const Wrapper = styled(motion.div)`
       max-width: 1000px;
     `}
   }
-
-  div:nth-child(4n) {
-    text-align: right;
-    align-self: flex-end;
-  }
-
-  div:nth-child(4n - 1) {
-    text-align: right;
-  }
-
-  div:first-child {
-    margin-top: -50px;
-  }
-
-  div:last-child {
-    margin-bottom: -20px;
-
-    ${({ theme }) => theme.media.desktop`
-      margin-bottom: 30px;
-    `}
-  }
 `;
 
 export default { Wrapper };
--- src/components/molecules/BlogPostContentContent/BlogPostContentContent.styles.js
+++ src/components/molecules/BlogPostContentContent/BlogPostContentContent.styles.js
@@ -3,6 +3,27 @@ import styled from 'styled-components';
 const Content = styled.div`
   display: flex;
   flex-direction: column;
+
+  div:nth-child(4n) {
+    text-align: right;
+    align-self: flex-end;
+  }
+
+  div:nth-child(4n - 1) {
+    text-align: right;
+  }
+
+  div:first-child {
+    margin-top: -50px;
+  }
+
+  div:last-child {
+    margin-bottom: -20px;
+
+    ${({ theme }) => theme.media.desktop`
+      margin-bottom: 30px;
+    `}
+  }
 `;
 
 export default { Content };
