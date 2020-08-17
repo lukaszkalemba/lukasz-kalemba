af63871c2bed529475af8d85bf4eb4cd02c374a1
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-08-17 13:53:47 +0200

    Create stories for services wrapper component

---
+++ src/components/organisms/PricingContent/PricingContent.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PricingContent from './PricingContent';
+
+export const pricingContent = () => <PricingContent />;
+
+export default {
+  component: PricingContent,
+  title: 'Organisms|PricingContent',
+};
