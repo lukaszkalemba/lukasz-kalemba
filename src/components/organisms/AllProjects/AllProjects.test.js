b27a3335990f59d5625acd3cf29a9324952fe624
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-19 14:20:57 +0200

    Create test for all projects component

---
+++ src/components/organisms/AllProjects/AllProjects.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { render } from 'utils/test-utils';
+import * as Gatsby from 'gatsby';
+import '__mocks__/intersectionObserver';
+import AllProjects from './AllProjects';
+
+beforeEach(() => {
+  const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
+
+  useStaticQuery.mockImplementation(() => ({
+    projects: {
+      edges: [
+        {
+          node: {
+            id: 0,
+            title: 'fake blog post 0',
+            slug: 'fake-slug-0',
+            image: {
+              fluid: {
+                aspectRatio: 0,
+                sizes: '',
+                src: '',
+                srcSet: '',
+              },
+            },
+          },
+        },
+        {
+          node: {
+            id: 1,
+            title: 'fake blog post 1',
+            slug: 'fake-slug-1',
+            image: {
+              fluid: {
+                aspectRatio: 0,
+                sizes: '',
+                src: '',
+                srcSet: '',
+              },
+            },
+          },
+        },
+      ],
+    },
+  }));
+});
+
+describe('<AllProjects />', () => {
+  it('renders correctly', () => {
+    const { getByRole, getAllByRole } = render(<AllProjects />);
+
+    const pageHeading = getByRole('heading', { level: 1 });
+
+    const cards = getAllByRole('article');
+
+    expect(pageHeading).toBeInTheDocument();
+    expect(cards).toHaveLength(2);
+  });
+});
