0b77c866382d21b79ac039d2118442a72ac6ebf7
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-19 21:52:51 +0200

    Create test for blog post content component

---
+++ src/components/organisms/BlogPostContent/BlogPostContent.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'utils/test-utils';
+import '__mocks__/intersectionObserver';
+import BlogPostContent from './BlogPostContent';
+
+describe('<BlogPostContent />', () => {
+  it('renders correctly with string content given', () => {
+    const fakeContent =
+      '<h2>Znaczenie brandingu</h2><p>Lorem ipsum dolor sit amet</p><h2>Znaczenie brandingu</h2><p>Lorem ipsum dolor sit amet</p>';
+
+    const { getAllByTestId } = render(
+      <BlogPostContent content={fakeContent} />
+    );
+
+    const blogPostContentItems = getAllByTestId('blog-post-content-item');
+
+    expect(blogPostContentItems).toHaveLength(4);
+  });
+});
