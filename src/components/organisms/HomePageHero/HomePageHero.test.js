f816a87e8a539583c50fba9a3f25ed243151e88a
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-19 19:19:21 +0200

    Create test for home page hero component

---
+++ src/components/organisms/HomePageHero/HomePageHero.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'utils/test-utils';
+import '__mocks__/intersectionObserver';
+import HomePageHero from './HomePageHero';
+
+describe('<HomePageHero />', () => {
+  it('renders correctly with all children', () => {
+    const { getByText } = render(<HomePageHero />);
+
+    const heading = getByText(/zaistniej w sieci/i);
+    const paragraph = getByText(/zaistniej w świecie/i);
+    const pricingButton = getByText(/wycena/i);
+    const projectsButton = getByText(/projekty/i);
+    const scrollArrowInfo = getByText(/przesuń/i);
+
+    expect(heading).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+    expect(pricingButton).toBeInTheDocument();
+    expect(projectsButton).toBeInTheDocument();
+    expect(scrollArrowInfo).toBeInTheDocument();
+  });
+});
