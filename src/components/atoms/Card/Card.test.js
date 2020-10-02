bc8df5ef3ed418e8271139e3e27ebabedbdde0e6
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-02 16:27:45 +0200

    Refactor tests of card component

--- src/components/atoms/Card/Card.test.js
+++ src/components/atoms/Card/Card.test.js
@@ -4,9 +4,15 @@ import 'jest-styled-components';
 import '__mocks__/intersectionObserver';
 import Card from './Card';
 
-const fakeProps = {
+const renderCard = props => {
+  const utils = render(<Card {...props} />);
+
+  return { ...utils };
+};
+
+const cardProps = {
   index: 1,
-  title: 'Card title',
+  title: 'Amazon',
   image: {
     fluid: {
       aspectRatio: 0,
@@ -15,32 +21,35 @@ const fakeProps = {
       srcSet: '',
     },
   },
-  preSlug: 'pre-slug',
-  slug: 'slug',
+  preSlug: 'projekty',
+  slug: 'amazon',
 };
 
 describe('<Card />', () => {
-  it('renders correctly default variant', () => {
-    const { getByRole } = render(<Card {...fakeProps} />);
+  it('renders properly heading, link and article', () => {
+    const { getByRole } = renderCard(cardProps);
 
-    const cardTitle = getByRole('heading');
-    const cardLink = getByRole('link');
-    const cardArticle = getByRole('article');
-
-    expect(cardTitle).toHaveTextContent(fakeProps.title);
-    expect(cardLink).toHaveAttribute(
+    expect(getByRole('heading')).toHaveTextContent(cardProps.title);
+    expect(getByRole('link')).toHaveAttribute(
       'href',
-      `/${fakeProps.preSlug}/${fakeProps.slug}`
+      `/${cardProps.preSlug}/${cardProps.slug}`
     );
-
-    expect(cardArticle).toHaveStyleRule('height', '450px');
   });
 
-  it('renders correctly small variant', () => {
-    const { getByRole } = render(<Card {...fakeProps} small />);
+  it('has proper styles if variant is set to small', () => {
+    const { getByRole } = renderCard(cardProps);
 
-    const cardArticle = getByRole('article');
+    expect(getByRole('article')).toHaveStyleRule('height', '450px');
+  });
 
-    expect(cardArticle).toHaveStyleRule('height', '300px');
+  it('has proper styles if variant is set to small', () => {
+    const smallCardProps = {
+      ...cardProps,
+      small: true,
+    };
+
+    const { getByRole } = renderCard(smallCardProps);
+
+    expect(getByRole('article')).toHaveStyleRule('height', '300px');
   });
 });
