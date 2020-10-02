a3cd2bcead53e5a73b36f5b4677986ea23358bc2
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-02 17:41:28 +0200

    Refactor tests of nav item component

--- src/components/atoms/NavItem/NavItem.test.js
+++ src/components/atoms/NavItem/NavItem.test.js
@@ -3,22 +3,37 @@ import { render } from 'utils/test-utils';
 import user from '@testing-library/user-event';
 import NavItem from './NavItem';
 
+const renderNavItem = props => {
+  const utils = render(<NavItem {...props} />);
+
+  const navItemLink = utils.getByRole('link');
+  return { ...utils, navItemLink };
+};
+
 describe('<NavItem />', () => {
-  it('renders correctly with given props', () => {
-    const fakeProps = {
-      path: '/test',
-      name: 'test name',
+  it('renders with proper props', () => {
+    const navItemProps = {
+      path: '/projects',
+      name: 'projects',
       closeNav: jest.fn(),
     };
 
-    const { getByRole } = render(<NavItem {...fakeProps} />);
+    const { navItemLink } = renderNavItem(navItemProps);
 
-    const navItemLink = getByRole('link');
+    expect(navItemLink).toHaveTextContent(navItemProps.name);
+    expect(navItemLink).toHaveAttribute('href', navItemProps.path);
+  });
+
+  it('fires closing nav function on click', () => {
+    const navItemProps = {
+      path: '/projects',
+      name: 'projects',
+      closeNav: jest.fn(),
+    };
+
+    const { navItemLink } = renderNavItem(navItemProps);
 
     user.click(navItemLink);
-
-    expect(fakeProps.closeNav).toHaveBeenCalledTimes(1);
-    expect(navItemLink).toHaveTextContent(fakeProps.name);
-    expect(navItemLink).toHaveAttribute('href', fakeProps.path);
+    expect(navItemProps.closeNav).toHaveBeenCalledTimes(1);
   });
 });
