99deaf191f86bbb8bd39a5157524e0afac35e9bc
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-02 16:50:13 +0200

    Refactor tests of heading component

--- src/components/atoms/Heading/Heading.test.js
+++ src/components/atoms/Heading/Heading.test.js
@@ -5,26 +5,32 @@ import 'jest-styled-components';
 import '__mocks__/intersectionObserver';
 import Heading from './Heading';
 
-describe('<Heading />', () => {
-  it('renders correctly as h1 home page element', () => {
-    const fakeContent = 'heading content';
-    const { getByRole } = render(
-      <Heading tag="h1" homePage>
-        {fakeContent}
-      </Heading>
-    );
+const renderHeading = props => {
+  const utils = render(<Heading {...props} />);
 
-    const heading = getByRole('heading');
+  const heading = utils.getByRole('heading');
+  return { ...utils, heading };
+};
+
+describe('<Heading />', () => {
+  it('renders as h1 home page element', () => {
+    const headingProps = {
+      tag: 'h1',
+      homePage: true,
+      children: 'Home page',
+    };
+
+    const { heading } = renderHeading(headingProps);
 
     expect(heading.tagName).toBe('H1');
-    expect(heading).toHaveTextContent(fakeContent);
+    expect(heading).toHaveTextContent(headingProps.children);
     expect(heading).toHaveStyleRule('font-size', theme.font.size.m);
   });
 
-  it('renders correctly as h2 element', () => {
-    const { getByRole } = render(<Heading>heading content</Heading>);
-
-    const heading = getByRole('heading');
+  it('renders as h2 element', () => {
+    const { heading } = renderHeading({
+      children: 'Projects',
+    });
 
     expect(heading.tagName).toBe('H2');
     expect(heading).toHaveStyleRule('font-size', theme.font.size.s);
