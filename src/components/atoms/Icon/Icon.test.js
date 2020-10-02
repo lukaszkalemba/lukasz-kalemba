7edf1f9c60cae8eb979c97d7fb684e9642b72e25
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-02 16:54:33 +0200

    Refactor tests of icon component

--- src/components/atoms/Icon/Icon.test.js
+++ src/components/atoms/Icon/Icon.test.js
@@ -3,11 +3,11 @@ import { render } from 'utils/test-utils';
 import Icon from './Icon';
 
 describe('<Icon />', () => {
-  it('renders correctly and matches snapshot', () => {
-    const fakeSvgPath =
+  it('matches snapshot', () => {
+    const svhPath =
       'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjUiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCA2NSAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik0wIDFINjFMNDIuNSAxNC41IiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjIiLz4NCjwvc3ZnPg0K';
 
-    const { container } = render(<Icon src={fakeSvgPath} />);
+    const { container } = render(<Icon src={svhPath} />);
 
     expect(container.firstChild).toMatchSnapshot();
   });
