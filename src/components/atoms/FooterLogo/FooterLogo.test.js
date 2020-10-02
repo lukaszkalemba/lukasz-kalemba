8fee70969b66b417592fd4be4b10904802b8bd94
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-02 16:32:05 +0200

    Refactor tests of footer logo component

--- src/components/atoms/FooterLogo/FooterLogo.test.js
+++ src/components/atoms/FooterLogo/FooterLogo.test.js
@@ -3,7 +3,7 @@ import { render } from 'utils/test-utils';
 import FooterLogo from './FooterLogo';
 
 describe('<FooterLogo />', () => {
-  it('renders correctly and matches snapshot', () => {
+  it('matches snapshot', () => {
     const { container } = render(<FooterLogo />);
 
     expect(container.firstChild).toMatchSnapshot();
