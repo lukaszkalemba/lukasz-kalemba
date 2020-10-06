904584320f9e2b22cba106e4a4b6da073f5b0b1c
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-06 18:32:06 +0200

    Edit home page hero animations on mobile

--- src/components/molecules/HeroContent/HeroContent.animations.js
+++ src/components/molecules/HeroContent/HeroContent.animations.js
@@ -1,21 +1,25 @@
-const heading = {
-  variants: {
-    initial: { opacity: 0, x: -80 },
-    animate: { opacity: 1, x: 0, transition: { duration: 0.75 } },
-  },
+const getHeading = size => {
+  return {
+    variants: {
+      initial: { opacity: 0, x: size.width > 1150 ? 80 : -80 },
+      animate: { opacity: 1, x: 0, transition: { duration: 0.75 } },
+    },
+  };
 };
 
-const paragraph = {
-  variants: {
-    initial: { opacity: 0, x: -80 },
-    animate: { opacity: 0.5, x: 0, transition: { duration: 0.75 } },
-  },
+const getParagraph = size => {
+  return {
+    variants: {
+      initial: { opacity: 0, x: size.width > 1150 ? -80 : 80 },
+      animate: { opacity: 0.5, x: 0, transition: { duration: 0.75 } },
+    },
+  };
 };
 
 const getButtons = size => {
   return {
     variants: {
-      initial: { opacity: 0, x: -80 },
+      initial: { opacity: 0, x: size.width > 1150 ? -80 : 80 },
       animate: {
         opacity: 1,
         x: 0,
@@ -29,7 +33,7 @@ const getButtons = size => {
 };
 
 export default {
-  heading,
-  paragraph,
+  getHeading,
+  getParagraph,
   getButtons,
 };
--- src/components/molecules/HeroContent/HeroContent.js
+++ src/components/molecules/HeroContent/HeroContent.js
@@ -7,15 +7,18 @@ import S from './HeroContent.styles';
 const HeroContent = () => {
   const size = useWindowSize();
 
+  const headingAnimation = animations.getHeading(size);
+  const paragraphAnimation = animations.getParagraph(size);
   const buttonsAnimation = animations.getButtons(size);
+
   return (
     <S.Container axis="both">
       <div>
         <S.Content>
-          <S.Heading tag="h1" homePage animation={animations.heading}>
+          <S.Heading tag="h1" homePage {...headingAnimation}>
             Zaistniej w sieci
           </S.Heading>
-          <S.Paragraph animation={animations.paragraph}>
+          <S.Paragraph animation={paragraphAnimation}>
             <span>Zaistniej w świecie&nbsp;</span>
             <span>nieograniczonych możliwości</span>
           </S.Paragraph>
