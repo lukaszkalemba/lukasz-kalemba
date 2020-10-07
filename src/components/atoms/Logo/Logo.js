9d96bee118bffa74cd9778b3d7250b96bd5e1a02
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-07 16:42:12 +0200

    Fix disabled scrolling bug if logo was clicked

--- src/components/atoms/Logo/Logo.js
+++ src/components/atoms/Logo/Logo.js
@@ -2,15 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from 'components/atoms/Icon';
 import logo from 'assets/svgs/logo_lukaszkalemba.svg';
+import { enableScrolling } from 'helpers/enableScrolling';
 import S from './Logo.styles';
 
-const Logo = ({ path, setIsNavOpen }) => (
-  <S.Wrapper path={path} data-testid="logo" onClick={() => setIsNavOpen(false)}>
-    <S.Link to="/" aria-label="lukaszkalemba">
-      <Icon src={logo} />
-    </S.Link>
-  </S.Wrapper>
-);
+const Logo = ({ path, setIsNavOpen }) => {
+  const handleLogoClick = () => {
+    setIsNavOpen(false);
+    enableScrolling();
+  };
+
+  return (
+    <S.Wrapper path={path} data-testid="logo" onClick={handleLogoClick}>
+      <S.Link to="/" aria-label="lukaszkalemba">
+        <Icon src={logo} />
+      </S.Link>
+    </S.Wrapper>
+  );
+};
 
 Logo.propTypes = {
   path: PropTypes.string.isRequired,
