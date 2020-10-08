e0b984288efe156bd19bcad6e67b4942ee773137
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-08 19:59:52 +0200

    Create triangle decorator component

--- src/components/organisms/HomePageHero/HomePageHero.js
+++ src/components/organisms/HomePageHero/HomePageHero.js
@@ -1,24 +1,19 @@
 import React, { useEffect } from 'react';
 import HeroContent from 'components/molecules/HeroContent';
-import useWindowSize from 'hooks/useWindowSize';
+import TriangleDecorator from 'components/atoms/TriangleDecorator';
 import { setHeightOnMobile } from 'helpers/setHeightOnMobile';
 import animations from './HomePageHero.animations';
 import S from './HomePageHero.styles';
 
 const HomePageHero = () => {
-  const size = useWindowSize();
-
   useEffect(() => {
     setHeightOnMobile();
   }, []);
 
-  const isMobile = size.width < 1150;
-
-  const triangleAnimations = animations.getTriangle(isMobile);
   return (
     <S.Wrapper {...animations.wrapper}>
       <HeroContent />
-      <S.TriangleDecorator {...triangleAnimations} />
+      <TriangleDecorator />
     </S.Wrapper>
   );
 };
--- src/components/organisms/NotFoundSection/NotFoundSection.js
+++ src/components/organisms/NotFoundSection/NotFoundSection.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NotFoundSectionContent from 'components/molecules/NotFoundSectionContent';
+import TriangleDecorator from 'components/atoms/TriangleDecorator';
 import Container from 'components/particles/Container';
+import { setHeightOnMobile } from 'helpers/setHeightOnMobile';
 import S from './NotFoundSection.styles';
 
 const NotFoundSection = () => {
+  useEffect(() => {
+    setHeightOnMobile();
+  }, []);
+
   return (
     <S.Wrapper>
       <Container axis="both">
         <NotFoundSectionContent />
       </Container>
+      <TriangleDecorator />
     </S.Wrapper>
   );
 };
