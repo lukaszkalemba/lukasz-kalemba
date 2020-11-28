7c74a2ca33f5a9b834d350d0eb3d36e60059379e
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-11-28 11:11:35 +0100

    Install and use react-hotjar library

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 import SEO from 'components/particles/SEO';
 import HomePageHero from 'components/organisms/HomePageHero';
 import FeaturedProjects from 'components/organisms/FeaturedProjects';
 import OfferedServices from 'components/organisms/OfferedServices';
 import FeaturedBlogPosts from 'components/organisms/FeaturedBlogPosts';
 
-const IndexPage = () => (
-  <>
-    <SEO title="home" />
-    <HomePageHero />
-    <FeaturedProjects />
-    <OfferedServices />
-    <FeaturedBlogPosts />
-  </>
-);
+const IndexPage = () => {
+  useEffect(() => {
+    hotjar.initialize(process.env.HOTJAR_USER_ID, 6);
+  }, []);
+
+  return (
+    <>
+      <SEO title="home" />
+      <HomePageHero />
+      <FeaturedProjects />
+      <OfferedServices />
+      <FeaturedBlogPosts />
+    </>
+  );
+};
 
 export default IndexPage;
